e868cb001a7f790d4c582ec405b3bc5ae0178ca8
Cameron Barnes <ccbarnes88@gmail.com>
2019-02-05 21:04:20 -0500

    minor alignment fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import logo from './logo.png';
 import './App.css';
 
@@ -7,15 +7,11 @@ class App extends Component {
   render() {
     return (
       <Router className="App">
-        <nav>
-          <div className="logo-home">
-            <Link to="/"><img src={logo} alt="logo" width="200px"/></Link>
-          </div>
-          <div className="page-links">
-            <Link to="/about">About</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/contact">Contact Us</Link>
-          </div>
+        <nav className="primary-nav">
+          <Link to="/" className="logo-home"><img src={logo} alt="logo" width="200px"/></Link>
+          <Link to="/about" clasName="page-links">About</Link>
+          <Link to="/services" clasName="page-links">Services</Link>
+          <Link to="/contact" clasName="page-links">Contact Us</Link>
         </nav>
       </Router>
     );
