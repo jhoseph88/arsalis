2c8f36c2ddebde12eb4e6c9ad6760c471f4630dc
Cameron Barnes <ccbarnes88@gmail.com>
2019-02-05 20:41:43 -0500

    add rudimentary navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router className="App">
+        <nav>
+          <div className="logo-home">
+            <Link to="/"><img src={logo} alt="logo" width="200px"/></Link>
+          </div>
+          <div className="page-links">
+            <Link to="/about">About</Link>
+            <Link to="/services">Services</Link>
+            <Link to="/contact">Contact Us</Link>
+          </div>
+        </nav>
+      </Router>
     );
   }
 }
