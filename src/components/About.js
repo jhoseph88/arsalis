7e372f27c505671003422b3bc4087d4e8dfbe898
Cameron Barnes <ccbarnes88@gmail.com>
2019-02-17 15:51:55 -0500

    wrap sideboxes on small screens

--- src/components/About.js
+++ src/components/About.js
@@ -17,45 +17,39 @@ export default function About(props) {
       <Divider text="Our Mission"/>
       <div className="side-box">
         <i className="fas fa-handshake" style={iconStyle}/>
-        <div className="trans-box">
-          <p>
-            We are committed to providing elegant, contemporary, affordable
-            software solutions to those who have yet to hop on the tech bandwagon.
-             We believe cloud computing lowers the barrier of entry for smaller
-             organizations who lack the capital or the expertise to hire a
-             full-time engineering team.
-          </p>
-        </div>
+        <p className="trans-box">
+          We are committed to providing elegant, contemporary, affordable
+          software solutions to those who have yet to hop on the tech bandwagon.
+           We believe cloud computing lowers the barrier of entry for smaller
+           organizations who lack the capital or the expertise to hire a
+           full-time engineering team.
+        </p>
       </div>
       <Divider text="Our Values"/>
       <div className="side-box">
-        <div className="trans-box">
-          <p>
-            We believe in getting to know our clients as individuals is a key
-            prerequisite to building for them products they can understand,
-            take pride in, and love. We see the ability to explain our systems
-            to a less technical audience than us as part of the definition of
-            a quality finished product.
-          </p>
-        </div>
+        <p className="trans-box">
+          We believe in getting to know our clients as individuals is a key
+          prerequisite to building for them products they can understand,
+          take pride in, and love. We see the ability to explain our systems
+          to a less technical audience than us as part of the definition of
+          a quality finished product.
+        </p>
         <i className="fas fa-hand-holding-heart" style={iconStyle}/>
       </div>
       <Divider text="Our Background"/>
       <div className="side-box">
         <i className="fas fa-people-carry" style={iconStyle}/>
-        <div className="trans-box">
-          <p>
-            We are a small team of colleagues and friends who have come together
-            under the common mission of sharing world-class technical systems to
-            regular people like us. We have worked full-time for Fortune 500
-            companies in fields including financial technology, healtchare,
-            insurance, eCommerce, marketing, and small business. As technologists
-            who have surveyed the state-of-the-art, we are prepared to bring to
-            you years of hard-earned experience as well as a committment to stay
-            up-to-date on the bleeding edge of scale, affordability, and
-            simplicity.
-          </p>
-        </div>
+        <p className="trans-box">
+          We are a small team of colleagues and friends who have come together
+          under the common mission of sharing world-class technical systems to
+          regular people like us. We have worked full-time for Fortune 500
+          companies in fields including financial technology, healtchare,
+          insurance, eCommerce, marketing, and small business. As technologists
+          who have surveyed the state-of-the-art, we are prepared to bring to
+          you years of hard-earned experience as well as a committment to stay
+          up-to-date on the bleeding edge of scale, affordability, and
+          simplicity.
+        </p>
       </div>
     </div>
   )
