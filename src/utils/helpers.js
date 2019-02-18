ac2a51fe4d2ed003b7550d0b839d31062fca5294
Shamim Ahmed <shamimahmedus@gmail.com>
2019-02-18 09:08:34 -0500

    created content json

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
+import Content from "./content.json"
+
 export function scrollToRef(ref) {
   window.scrollTo({ top: ref.current.offsetTop, behavior: 'smooth' })
+}
+
+export function getContent() {
+  return JSON.parse(Content)
 }
